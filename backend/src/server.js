af4c4ba532cba4ff5bb133e17637de3f11a61b64
vasthav18 <vasthavsri012@gmail.com>
2025-11-02 01:34:45 +0530

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,25 +5,29 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import  cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express()
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve()
 
 // connectDB();
 
 //middleware
+if (process.env.NODE_ENV != "production") {
 app.use(cors({
     origin:"http://localhost:5173",
 })
  );
+}
 app.use(express.json()); //this middleware will parse JSON bodies:req.body
 app.use(rateLimiter);
-app.use(cors({
-    origin:"http://localhost:5173",
-})
- );
+// app.use(cors({
+//     origin:"http://localhost:5173",
+// })
+//  );
 //our simple custom middleware
 // app.use((req,res,next) => {
 //     console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
@@ -31,6 +35,14 @@ app.use(cors({
 // });
 
 app.use("/api/notes",notesRoutes); 
+
+if(process.env.NODE_ENV === "production") {
+app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+app.get("*",(req,res) => {
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+});
+}
 
 connectDB().then(()=> {
 app.listen(PORT,() => {
